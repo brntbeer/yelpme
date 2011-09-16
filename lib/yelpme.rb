ce7a4793e5191a13f6ec6939e853a2e09bf22699
brntbeer <brent.beer@gmail.com>
2011-09-16 12:25:40 -0700

    Remove the Slop from the option parsing.
    Added in OptionParser, and a parsing method depending on options
    passed in.

--- lib/yelpme.rb
+++ lib/yelpme.rb
@@ -1,13 +1,13 @@
 require "yelpme/version"
 
+
 begin
   require 'rubygems'
 rescue LoadError
 end
 require 'yelp2'
 require 'pp'
-require 'slop'
-
+require 'optparse'
 module Yelpme
   
   # A quick search to get the first result. Additional arguments
@@ -25,19 +25,35 @@ module Yelpme
     term = args.shift
     location = {}
     location[:location] = args.shift
-    opts = Slop.parse(args) do
-      on :r, :random, 'Random result', :optional => true
-      on :V, :version, 'Print the version' do
-        puts "Version #{Yelpme::VERSION}"
-        exit
+    options = {}
+    OptionParser.new do |opts|
+      opts.on("-r", "--random", "Random result") do |r|
+        options[:random] = r
       end
-    end
+      opts.on("-V", "--version", "Print the version") do |v|
+        options[:version] = v
+      end
+    end.parse!
+
     raise ArgumentError, "Location needs a value" if location[:location].nil?
     if %w{YELP_CONSUMER_KEY YELP_CONSUMER_SECRET YELP_TOKEN YELP_TOKEN_SECRET}.all?{|word| ENV.include?(word)}
       query = Yelp::Base.new(ENV["YELP_CONSUMER_KEY"],ENV["YELP_CONSUMER_SECRET"],ENV["YELP_TOKEN"], ENV["YELP_TOKEN_SECRET"])
-      pp query.search(term, location)[0]
+      businesses = query.search(term, location)
+      business = parse_businesses(businesses, options)
+      pp business
     else
       raise LoadError, "Verify that your ENV variables for authentication to Yelp are set. View the README for more information."
     end
+  end
+
+  # Parse how the business should be parsed based upon the options
+  # are passed in. 
+  #
+  # businesses - Array of businesses returned from the Yelp search.
+  # options    - Options passed in from OptionParser.
+  #
+  # Returns one or many businesses.
+  def parse_businesses(businesses, options)
+    options[:random].nil? ?  businesses.first : businesses[rand(businesses.size)]
   end
 end
